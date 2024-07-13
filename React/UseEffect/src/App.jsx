e96fa8baadc359ab6f053d1312601d3b4310bb8f
Shankaranarayanansk <sankaranarayananit002@gmail.com>
2024-07-13 06:05:00 +0530

    r-2

--- React/UseEffect/src/App.jsx
+++ React/UseEffect/src/App.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const[data,setData] = useState(100)
-  useEffect(() =>{},[])
+  const[data,setData]=useState(9090);
+  const[naa,setNaa]=useState(200)
+
+  useEffect(() => {
+    setData(200)
+    console.log("first")
+
+    return(()=>{setData(100)//it is used to clean the memory and avoids performance issues
+      console.log("cleaned")
+    })
+  },[data,naa])
+  console.log(data)
+
   return (
     <div>
-      <h1>{data}</h1>
+
+      <h2>
+        {data}
+      </h2>
+      <button onClick={() => setData((prevData) => prevData + 1)}>
+        </button>
+        <p>{naa}</p>
+        <button onClick={()=>setNaa((date)=>date+1)}></button>
+
     </div>
   )
 }
